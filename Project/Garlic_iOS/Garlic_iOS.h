6c6ec67ec81ca824f963bdaf5b8e0fcce08f5c20
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2026-07-05 17:54:01 +0900

    build: Add install phase

---
+++ Project/Garlic_iOS/Garlic_iOS.h
@@ -0,0 +1,16 @@
+//
+//  Garlic_iOS.h
+//  Garlic_iOS
+//
+//  Created by Tomoo Hamada on 2026/07/05.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Garlic_iOS.
+FOUNDATION_EXPORT double Garlic_iOSVersionNumber;
+
+//! Project version string for Garlic_iOS.
+FOUNDATION_EXPORT const unsigned char Garlic_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Garlic_iOS/PublicHeader.h>
